c744106bbf1e9669cde3042c2dcb700f8120fc9e
Sooraj Philip <soorajphilip97@gmail.com>
2022-08-22 14:59:31 +0530

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users,
+              controllers: {
+                sessions: 'users/sessions',
+                registrations: 'users/registrations'
+              }
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
+end
